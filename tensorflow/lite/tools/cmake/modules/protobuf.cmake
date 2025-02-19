671ce2bcc08ad63811545e64453d2d8cdca64386
esaaprillia <esaapriliasalsabila@gmail.com>
2025-02-19 14:26:45 +0800

    Update protobuf.cmake

--- tensorflow/lite/tools/cmake/modules/protobuf.cmake
+++ tensorflow/lite/tools/cmake/modules/protobuf.cmake
@@ -13,30 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(OverridableFetchContent)
-
-OverridableFetchContent_Declare(
-  protobuf
-  GIT_REPOSITORY https://github.com/protocolbuffers/protobuf
-  # Sync with tensorflow/third_party/protobuf/protobuf.patch
-  GIT_TAG 90b73ac3f0b10320315c2ca0d03a5a9b095d2f66
-  GIT_PROGRESS TRUE
-  PREFIX "${CMAKE_BINARY_DIR}"
-  SOURCE_DIR "${CMAKE_BINARY_DIR}/protobuf"
-)
-
-set(protobuf_ABSL_PROVIDER "package" CACHE STRING "" FORCE)
-set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
-set(protobuf_INSTALL OFF CACHE BOOL "" FORCE)
-set(protobuf_WITH_ZLIB OFF CACHE BOOL "" FORCE)
-set(protobuf_BUILD_PROTOC_BINARIES ON CACHE BOOL "" FORCE)
-
-OverridableFetchContent_GetProperties(protobuf)
-if(NOT protobuf_POPULATED)
-  OverridableFetchContent_Populate(protobuf)
-endif()
-
 set(Protobuf_INCLUDE_DIR "${protobuf_SOURCE_DIR}/src" CACHE INTERNAL "")
 set(Protobuf_LIBRARIES protobuf::libprotobuf CACHE INTERNAL "")
 
