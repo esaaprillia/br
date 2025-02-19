769232597e8869edfaa91204506c52adab317925
esaaprillia <esaapriliasalsabila@gmail.com>
2025-02-19 11:54:48 +0800

    Update fft2d.cmake

--- tensorflow/lite/tools/cmake/modules/fft2d.cmake
+++ tensorflow/lite/tools/cmake/modules/fft2d.cmake
@@ -17,22 +17,6 @@ if(TARGET fft2d OR fft2d_POPULATED)
   return()
 endif()
 
-include(OverridableFetchContent)
-
-OverridableFetchContent_Declare(
-  fft2d
-  URL https://storage.googleapis.com/mirror.tensorflow.org/github.com/petewarden/OouraFFT/archive/v1.0.tar.gz
-  # Sync with tensorflow/workspace2.bzl
-  URL_HASH SHA256=5f4dabc2ae21e1f537425d58a49cdca1c49ea11db0d6271e2a4b27e9697548eb
-  SOURCE_DIR "${CMAKE_BINARY_DIR}/fft2d"
-  LICENSE_FILE "readme2d.txt"
-  LICENSE_URL "http://www.kurims.kyoto-u.ac.jp/~ooura/fft.html"
-)
-OverridableFetchContent_GetProperties(fft2d)
-if(NOT fft2d_POPULATED)
-  OverridableFetchContent_Populate(fft2d)
-endif()
-
 set(FFT2D_SOURCE_DIR "${fft2d_SOURCE_DIR}" CACHE PATH "fft2d source")
 add_subdirectory(
   "${CMAKE_CURRENT_LIST_DIR}/fft2d"
