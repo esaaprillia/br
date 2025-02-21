790297389ffe98ee8db7c8e1ae9232d4b18bfa3d
esaaprillia <esaapriliasalsabila@gmail.com>
2025-02-21 15:53:36 +0800

    Update fp16_headers.cmake

--- tensorflow/lite/tools/cmake/modules/fp16_headers.cmake
+++ tensorflow/lite/tools/cmake/modules/fp16_headers.cmake
@@ -18,23 +18,6 @@ if(TARGET fp16_headers OR fp16_headers_POPULATED OR TFLITE_ENABLE_XNNPACK)
   return()
 endif()
 
-include(OverridableFetchContent)
-
-OverridableFetchContent_Declare(
-  fp16_headers
-  GIT_REPOSITORY https://github.com/Maratyszcza/FP16
-  # Sync with https://github.com/google/XNNPACK/blob/master/cmake/DownloadFP16.cmake
-  GIT_TAG 0a92994d729ff76a58f692d3028ca1b64b145d91
-  GIT_PROGRESS TRUE
-  PREFIX "${CMAKE_BINARY_DIR}"
-  SOURCE_DIR "${CMAKE_BINARY_DIR}/fp16_headers"
-)
-
-OverridableFetchContent_GetProperties(fp16_headers)
-if(NOT fp16_headers)
-  OverridableFetchContent_Populate(fp16_headers)
-endif()
-
 include_directories(
   AFTER
    "${fp16_headers_SOURCE_DIR}/include"
