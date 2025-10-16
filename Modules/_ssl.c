a4948b0733940943990b493dde28c361a59cb92d
esaaprillia <esaapriliasalsabila@gmail.com>
2025-10-16 12:06:57 +0800

    Update _ssl.c

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -62,6 +62,7 @@
 #include "openssl/rsa.h"
 #include "openssl/crypto.h"
 #include "openssl/x509.h"
+#include "openssl/x509_vfy.h"
 #include "openssl/x509v3.h"
 #include "openssl/pem.h"
 #include "openssl/ssl.h"
